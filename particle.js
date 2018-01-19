c7cd91f7500c4477709260f476af68bcce22415b
Napageneral <Tnapathy@gmail.com>
2018-01-19 12:17:25 -0800

    Comments

--- particle.js
+++ particle.js
@@ -1,4 +1,5 @@
 function Particle(){
+  //initialize the particle somewhere randomly on screen
   this.pos=createVector(random(width),random(height));
   this.vel=createVector(0,0);
   this.acc=createVector(0,0);
@@ -6,25 +7,32 @@ function Particle(){
 
 
   this.update=function(){
+    //basic physics model
     this.vel.add(this.acc);
+    //limits the velocity to the maxspeed variable
     this.vel.limit(this.maxspeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
 
+  
+  //Causes the particle to be acted on by the perlin flow field
   this.follow=function(vectors){
+    //find the closest matching index of the 2D perlin field to the particle's position
     var x = floor(this.pos.x/scl);
     var y = floor(this.pos.y/scl);
+    //flatten the 2D index
     var index = x+y*cols;
+    //get the force from the Perlin field
     var force = vectors[index];
     this.applyForce(force);
   }
 
-
   this.applyForce=function(force){
     this.acc.add(force);
   }
-
+  
+  //renders the particle
   this.show= function(){
     push();
     strokeWeight(4);
@@ -33,6 +41,7 @@ function Particle(){
     pop();
   }
 
+  //edge detection for the particle that causes it to wrap around to the other side of the screen
   this.edges=function(){
     if (this.pos.x>width) {
       this.pos.x=0;
